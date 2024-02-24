db562ef4572d9f01e6c379038df820ff7c3cc1d1
Ankit Singh Bhatti <155420477+Ankit-Singh-Bhatti@users.noreply.github.com>
2024-02-24 21:29:55 +0530

    Delete LAB-6 directory

--- LAB-6/strget.java
+++
@@ -1,9 +0,0 @@
-class strget{
-    public static void main(String args[]){
-        String org="Welcome to BMSCE college";
-        int start=11,end=16;
-        char chars[]=new char[end-start];
-        org.getChars(start,end,chars,0);
-        System.out.println(chars);
-    }
-}
